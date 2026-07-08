6314fc3b7849898da63278b0b74354eb98cdefd2
akshxt25 <girdharakshat5@gmail.com>
2026-07-08 20:48:58 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="text-red-500">
+      If this text is red, Tailwind CSS is working correctly.
+    </div>
+  )
+}
+
+export default App
